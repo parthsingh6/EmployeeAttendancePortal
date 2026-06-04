11c00133e6cc6160162bfd1c776726bcbd7c3704
Parth Singh <parth060106@gmail.com>
2026-06-04 11:00:04 +0530

    Day 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main () {
-runApp(MyApp());
-}
+import 'home_page.dart';
 
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Material(
-      child: Center(
-        child: Container(   
-        child: Text("Welcome to the flutter"),
-        ),
-      ),
-      ),
-    );
+    int days = 30;
+    String name = "parth";
+
+    return MaterialApp(home: HomePage());
   }
 }
-    
